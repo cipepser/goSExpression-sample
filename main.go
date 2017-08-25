ddc61309a78b3ba686ce602833537077d67bc75c
cipepser <respepic@gmail.com>
2017-08-25 12:31:51 +0900

    implement Insert

--- main.go
+++ main.go
@@ -1,20 +1,18 @@
 package main
 
 import (
-	// "container/list"
-
 	"fmt"
 	"strings"
 )
 
 type Tree struct {
-	Parent, Child *Tree
-	Values        []string
+	Parent *Tree
+	Child  []*Tree
+	Value  string
 }
 
 func NewTree() *Tree {
-	t := new(Tree)
-	// t.Child = t
+	t := &Tree{}
 	t.Parent = t
 	return t
 }
@@ -35,98 +33,41 @@ func (t *Tree) Insert(s string) *Tree {
 		return t
 	}
 	s = strings.TrimSpace(s)
-	fmt.Println("-----------")
-	fmt.Println("t: ", &t)
-	fmt.Println("start: ", s)
-	fmt.Println("s[0]: ", string(s[0]))
 
 	switch s[0] {
 	case '(':
-		fmt.Println("t: ", t)
-		fmt.Println("t.Child: ", t.Child)
-		fmt.Println("t.Parent: ", t.Parent)
-
-		n := &Tree{}
-		n.Parent = t
+		n := &Tree{Parent: t}
 		n = n.Insert(s[1:])
-		t.Child = n
-		// t.Child = n.Insert(s[1:])
-		// t.Child.Parent = t
-		// fmt.Println(t.Child.Parent)
-
-		// t.Child = t.Child.Insert(s[1:])
-
+		t.Child = append(t.Child, n)
 	case ')':
-		if t.Parent == t {
-			return t
-		}
-		fmt.Println(":::::", t)
-		t.Parent.Insert(s[1:])
+		t.Parent = t.Parent.Insert(s[1:])
 
 	default:
-		t.Values = append(t.Values, string(s[0]))
-		fmt.Println(t.Values)
+		for i := 0; i < len(s); i++ {
 
-		t = t.Insert(s[1:])
+			if s[i] == '(' || s[i] == ')' {
+				t.Value = s[:i]
+				t = t.Insert(s[i:])
+				break
+			}
+
+		}
 	}
-
 	return t
 }
 
 func (t *Tree) Walk() {
-	for _, v := range t.Values {
-		fmt.Print(v, " ")
+	fmt.Println("my parent is ", t.Parent.Value)
+	fmt.Println(t.Value)
+	fmt.Println("-------")
+
+	for _, c := range t.Child {
+		c.Walk()
 	}
-	if t == t.Child || t == nil {
-		return
-	}
-	fmt.Println("")
-	t.Child.Walk()
 }
 
 func main() {
-	// 	s := `(ROOT
-	//     (S
-	//       (PP
-	//         (NP
-	//           (JJ Natural)
-	//           (NN language)
-	//           (NN processing)
-	//         )
-	//         (IN From)
-	//         (NP
-	//           (NNP Wikipedia)
-	//         )
-	//       )
-	//       (, ,)
-	//       (NP
-	//         (NP
-	//           (DT the)
-	//           (JJ free)
-	//           (NN encyclopedia)
-	//           (JJ Natural)
-	//           (NN language)
-	//           (NN processing)
-	//         )
-	//       )
-	//     )
-	//   )
-	// `
-	// s := `(ROOT (S (PP (NP (JJ Natural) (NN language) (NN processing)) )))`
-	// s := `(a(b(c(d(e)(f)(g)))))`
-	// s := `(a(b)(c)(d))`
-	s := `(a(b))`
-	// s := `(a)`
-
+	s := `(ROOT (S (PP (NP (JJ Natural) (NN language) (NN processing)) )))`
 	t, _ := Parse(s)
-	// fmt.Println(t)
-	// t := NewTree()
 	t.Walk()
-
-	// l := list.New()
-	// l.PushFront("a")
-	// l.PushFront("b")
-	// fmt.Println(l.PushFront("a"))
-	// fmt.Println(l)
-	// l.PushFront(v)
 }
